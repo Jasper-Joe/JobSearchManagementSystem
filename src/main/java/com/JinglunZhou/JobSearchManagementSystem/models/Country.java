9e94aa5894b08287573ccdcdd78d3c8f820e0619
jasper <jasperzhou1129@gmail.com>
2020-09-10 00:16:08 -0400

    models

--- src/main/java/com/JinglunZhou/JobSearchManagementSystem/models/Country.java
+++ src/main/java/com/JinglunZhou/JobSearchManagementSystem/models/Country.java
@@ -1,5 +1,51 @@
 package com.JinglunZhou.JobSearchManagementSystem.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class Country {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String capital;
+	private String nationality;
+	private String description;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 
 }
--- src/main/java/com/JinglunZhou/JobSearchManagementSystem/models/State.java
+++ src/main/java/com/JinglunZhou/JobSearchManagementSystem/models/State.java
@@ -1,5 +1,54 @@
 package com.JinglunZhou.JobSearchManagementSystem.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class State {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name="countryid",insertable=false,updatable=false)
+	private Country country;
+	private Integer countryid;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Country getCountry() {
+		return country;
+	}
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	public Integer getCountryid() {
+		return countryid;
+	}
+	public void setCountryid(Integer countryid) {
+		this.countryid = countryid;
+	}
 
 }
