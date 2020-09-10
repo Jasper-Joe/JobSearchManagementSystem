477f0677fd99743e2d5f178fec1fa87aafac646e
jasper <jasperzhou1129@gmail.com>
2020-09-09 22:56:31 -0400

    uploading my project

---
+++ src/main/java/com/JinglunZhou/JobSearchManagementSystem/JobSearchManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.JinglunZhou.JobSearchManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JinglunZhou/JobSearchManagementSystem/JobSearchManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JinglunZhou.JobSearchManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
